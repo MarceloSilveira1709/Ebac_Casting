b53f4fe6095da1ee5a287831fb1be846c809892e
MarceloSilveira <marcelosilverag@hotmail.com>
2024-02-22 09:57:32 -0300

    first commit

---
+++ src/Marcelo/Silveira/Primitivos.java
@@ -0,0 +1,15 @@
+package Marcelo.Silveira;
+
+public class Primitivos {
+
+    public static void main(String args []) {
+
+
+        int idade = 10;
+        long idadeL = idade;
+
+    System.out.println(idadeL);
+
+    }
+
+}
