2c0f4791ce072ac3b523bc7b29ea0b55f3b0d82b
MarceloSilveira <marcelosilverag@hotmail.com>
2024-02-22 10:29:57 -0300

    Refazendo exercicio

--- src/Marcelo/Silveira/Primitivos.java
+++ src/Marcelo/Silveira/Primitivos.java
@@ -5,10 +5,11 @@ public class Primitivos {
     public static void main(String args []) {
 
 
-        int idade = 10;
-        long idadeL = idade;
 
-    System.out.println(idadeL);
+        int idadeP = 10;
+        Integer idadeW = Integer.valueOf(idadeP);
+
+    System.out.println(idadeP);
 
     }
 
